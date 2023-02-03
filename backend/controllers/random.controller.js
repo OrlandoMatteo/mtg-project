19d12d66835350004343b8d48cbdcbb7c5fe630c
asdasd <asdasd>
2023-02-03 15:09:49 +0100

    Added bg colors

--- backend/controllers/random.controller.js
+++ backend/controllers/random.controller.js
@@ -1,11 +1,11 @@
 
 async function randomCard(_req, reply) {
   const collection = this.mongo.db.collection('cards')
-  const result = await collection.aggregate([{ $sample: { size: 1 } }]).toArray()
+  const result = await collection.aggregate([{ $sample: { size: 1 }},{ $project: {colors:1, name: 1, manaCost: 1, text: 1, "identifiers.scryfallOracleId": 1  } }]).toArray()
   if (result.length == 0) {
-  return { 'error': 'No foods found' }
-  } 
+    return { 'error': 'No foods found' }
+  }
   reply.send(result)
 }
 
-export  {randomCard}
+export { randomCard }
