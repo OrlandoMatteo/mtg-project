5e9f6743f7a9d84b60de5db3790f650bfd606914
asdasd <asdasd>
2023-02-06 14:22:10 +0100

    Update astro css

--- mtg-frontend/tailwind.config.cjs
+++ mtg-frontend/tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			dropShadow: {
+				'3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+			}
 		},
 	},
 	plugins: [],
